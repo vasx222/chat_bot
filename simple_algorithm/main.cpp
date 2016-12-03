945c9af275ab94cc151b4ec16a6bf8efaca648e0
Vasco <vasx222@mail.ru>
2016-12-03 03:30:04 +0300

    added parsing xml

--- simple_algorithm/main.cpp
+++ simple_algorithm/main.cpp
@@ -2,7 +2,7 @@
 #include <libxl.h>
 #include <string>
 using namespace libxl;
-
+using namespace std;
 void CreateSimpleXML()
 {
 	Book* book = xlCreateBook(); // xlCreateXMLBook() for xlsx
@@ -19,19 +19,28 @@ void CreateSimpleXML()
 	}
 }
 
-void ParseXMLQuestions()
+void ParseXMLQuestions(string * que, string * ans)
 {
 	Book * book = xlCreateXMLBook();
 	book->load("questions.xlsx");
 	Sheet * sheet = book->getSheet(0);
-	std::string str = sheet->readStr(1, 1);
-	std::cout << str << std::endl;
+
+	for (int i = 0; i < 40; i++)
+	{
+		que[i] = sheet->readStr(i + 1, 1);
+		ans[i] = sheet->readStr(i + 1, 2);
+	}
 	book->release();
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	ParseXMLQuestions();
+	string que[40], ans[40];
+	ParseXMLQuestions(que, ans);
+	for (int i = 0; i < 40; i++)
+	{
+		cout << "QUESTION: " << que[i] << "\nANSWER: " << ans[i] << "\n\n";
+	}
 	system("pause");
 }
