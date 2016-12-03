92a55a91c1ca3c55b518cf811755e5093823c5fc
Vasco <vasx222@mail.ru>
2016-12-03 03:19:35 +0300

    added working libxml

--- simple_algorithm/main.cpp
+++ simple_algorithm/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <libxl.h>
+#include <string>
+using namespace libxl;
+
+void CreateSimpleXML()
+{
+	Book* book = xlCreateBook(); // xlCreateXMLBook() for xlsx
+	if (book)
+	{
+		Sheet* sheet = book->addSheet("Sheet1");
+		if (sheet)
+		{
+			sheet->writeStr(2, 1, "Hello, World !");
+			sheet->writeNum(3, 1, 1000);
+		}
+		book->save("my_example.xls");
+		book->release();
+	}
+}
+
+void ParseXMLQuestions()
+{
+	Book * book = xlCreateXMLBook();
+	book->load("questions.xlsx");
+	Sheet * sheet = book->getSheet(0);
+	std::string str = sheet->readStr(1, 1);
+	std::cout << str << std::endl;
+	book->release();
+}
 
 int main()
 {
-	std::cout << "Hello, world!\n";
+	setlocale(LC_ALL, "Rus");
+	ParseXMLQuestions();
 	system("pause");
 }
